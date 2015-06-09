62ab7a64991c45659f93f9b1735076f777debcc9
Damien Storm <damien.r.storm@gmail.com>
2015-06-09 18:38:42 +0000

    added tstamp, dots, and no_dots methods

--- lib.rb
+++ lib.rb
@@ -1,7 +1,9 @@
 
 require 'watir-webdriver'
+require 'headless'
 require 'rubyXL'
 require './amazon_search.rb'
+require 'pry'
 
 def send_enter
 	@browser.send_keys :enter
@@ -9,4 +11,20 @@ end
 
 def read_workbook
 	@workbook = RubyXL::Parser.parse('data.xlsx')
+end
+
+def tstamp
+	Time.now.strftime("%Y.%m.%d-%H.%M.%S").to_s
+end
+
+def dots
+	@dots = Thread.new {loop {print ".";sleep 0.3333}}
+	@dots
+end
+
+def no_dots
+	unless @dots.nil?
+		Thread.kill(@dots)
+		puts ""
+	end
 end
