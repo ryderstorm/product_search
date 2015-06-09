27127bf4d1849348711f7244bb5b1fb400c28285
Damien Storm <damien.r.storm@gmail.com>
2015-06-09 18:33:11 -0400

    added methods for screenshots and error reports, added export data to xlsx workbook

--- amazon_search.rb
+++ amazon_search.rb
@@ -2,9 +2,14 @@ def amazon_search
 	@error = false
 	begin
 		puts "Beginning Amazon Search"
+		@temp_folder = "./amazon_temp/#{tstamp}"
+		FileUtils.mkdir_p(@temp_folder)
+		puts "Temp folder location: #{File.absolute_path(@temp_folder)}"
 		dots
-		temp_folder = "./amazon_temp/#{tstamp}"
-		Dir.mkdir(temp_folder)
+		result_urls = []
+		result_counts = []
+		workbook = RubyXL::Workbook.new
+		workbook_location = "#{@temp_folder}/AMAZON_DATA_#{tstamp}.xlsx"
 		skus = File.read('amazon_skus.txt').split("\n")
 		if @headless
 			headless = Headless.new
@@ -15,45 +20,72 @@ def amazon_search
 		browser.window.resize_to(900, 1000)
 		browser.window.move_to(0, 0)
 		browser.goto 'http://www.amazon.com'
-		results = []
 
 		searchbox = browser.text_field(id:'twotabsearchtextbox')
 		skus.each_with_index do |item, i|
+
+			#search for the current sku
 			sku, desc = item.split('|')
 			puts "\nprocessing sku [#{sku} | #{desc}]"
-			searchbox.set
+			worksheet = workbook.add_worksheet(sku)
+			worksheet.add_cell(0, 0, sku)
+			worksheet.add_cell(0, 1, desc)
+			searchbox.set sku
 			send_enter
 			sleep 1
 			browser.div(id:'navFooter').wait_until_present
-			if browser.element(id:'noResultsTitle').present?
-				results.push "#{i+1}. | #{sku.center(20)} | #{desc} | NO RESULTS FOUND"
+			worksheet.add_cell(1, 0, "Search results URL")
+			worksheet.add_cell(1, 1, browser.url)
+			worksheet.add_cell(1, 2, take_screenshot("#{sku}_SEARCH_RESULTS"))
+
+			# move on if no results were found
+			if browser.element(id:'noresult_countsTitle').present?
+				result_counts.push "#{i+1}. | #{sku.center(20)} | #{desc} | NO result_counts FOUND"
 				next
 			end
-			# browser.div(id:'searchTemplate').wait_until_present
-			# browser.element(id:'s-result-count').wait_until_present
-			num_results = browser.element(id:'s-result-count').text.split('of ').last.split(' results').first
-			results.push "#{i+1}. | #{sku.center(20)} | #{desc} | #{num_results.center(10)}"
-			# browser.li(id:'result_0').flash
+
+			# get the number of results
+			search_result_counts = browser.element(id:'s-result-count').text
+			if search_result_counts.include?('1 result')
+					number_results = 1
+			elsif search_result_counts.include?(' of ')
+				number_results = search_result_counts.split('of ').last.split(' results').first
+			else 
+				number_results = search_result_counts.split(' ').first
+			end
+			worksheet.add_cell(2, 0, "Number of search results")
+			worksheet.add_cell(2, 1, number_results)
+			worksheet.add_cell(2, 2, save_image("#{sku}_SEARCH_THUMBNAIL", browser.li(id:'result_0').imgs.first.attribute_value('src')))
+
+			# go to the first result
+			browser.li(id:'result_0').links.first.click
+			sleep 1
+			browser.div(id:'navFooter').wait_until_present
+
+			# save the workbook
+			worksheet.change_column_width(0, 30) 
+			worksheet.change_column_width(1, 30) 
+			worksheet.change_column_width(2, 30) 
+			workbook.write(workbook_location)
 		end
 		no_dots
 		puts "\n==============="
-		results.each { |r| puts r}
+		result_counts.each { |r| puts r }
 		puts "===============\n"
 		puts "finished processing"
 	rescue Exception => e
 		@error = true
 		no_dots
-		error_file = "#{temp_folder}/ERROR_#{tstamp}.png"
-		browser.screenshot.save error_file
-		puts "\n!!!!!!!!!!!!!!!!!!!!!\nAn error has occurred:"
-		puts e.backtrace.first
-		puts "\t#{e.message}"
-		puts "!!!!!!!!!!!!!!!!!!!!!\n"
+		error_file = take_screenshot('ERROR')
+		error_report(e)
 		puts "Exiting after fail due to error. Screenshot saved as [#{error_file}]"
 		binding.pry
 	end
 	browser.close
 	headless.destroy if @headless
 	no_dots
-	puts "Amazon scrape completed succesfully." unless @error
+	unless @error
+		puts "Amazon scrape completed succesfully." 
+		system(workbook_location)
+	end
 end
--- lib.rb
+++ lib.rb
@@ -4,8 +4,9 @@ require 'headless'
 require 'rubyXL'
 require './amazon_search.rb'
 require 'pry'
+require 'open-uri'
 
-def send_enter
+def send_enter	
 	@browser.send_keys :enter
 end
 
@@ -27,4 +28,47 @@ def no_dots
 		Thread.kill(@dots)
 		puts ""
 	end
+end
+
+def take_screenshot(filename = 'screenshot')
+	complete_name = "#{@temp_folder}/#{filename}_#{tstamp}.png"
+	@browser.screenshot.save complete_name
+	complete_name
+end
+
+def error_report(e)
+	puts "\n!!!!!!!!!!!!!!!!!!!!!\nAn error has occurred:"
+	puts "#{e.message}"
+	(0..8).each { |i| puts "\t" + e.backtrace[i] }
+	puts "!!!!!!!!!!!!!!!!!!!!!\n"
+end
+
+def pluralize(number)
+	number == 1 ? (return ""):(return "s")
+end
+
+def seconds_to_string(s)
+	# d = days, h = hours, m = minutes, s = seconds
+	m = (s / 60).floor
+	s = (s % 60).floor
+	h = (m / 60).floor
+	m = m % 60
+	d = (h / 24).floor
+	h = h % 24
+
+	output = "#{s} second#{pluralize(s)}" if (s > 0)
+	output = "#{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (m > 0)
+	output = "#{h} hour#{pluralize(h)}, #{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (h > 0)
+	output = "#{d} day#{pluralize(d)}, #{h} hour#{pluralize(h)}, #{m} minute#{pluralize(m)}, #{s} second#{pluralize(s)}" if (d > 0)
+
+	return output
+end
+
+def save_image(name, src)
+	extension = src.split('.').last
+	complete_name = "#{@temp_folder}/#{name}_#{tstamp}.#{extension}"
+	File.open(complete_name, 'wb') do |f|
+	  f.write open(src).read
+	end
+	complete_name
 end
