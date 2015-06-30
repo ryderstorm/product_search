054f8545f2bb6e2589dd4199b7b351d799d55e89
Damien Storm <damien.r.storm@gmail.com>
2015-06-30 11:53:43 -0400

    changing data size to 500 for testing on digital-ocean

--- libraries/main.rb
+++ libraries/main.rb
@@ -12,6 +12,7 @@ def init_variables
 	@run_stamp = tstamp
 	if @computer.include?('digital-ocean') 
 		@amazon_data = File.absolute_path('data/amazon.xlsx')
+		@amazon_data = File.absolute_path('data/amazon_test_big.xlsx')
 		@group_size = 10
 	else
 		@amazon_data = File.absolute_path('data/amazon_test.xlsx')
