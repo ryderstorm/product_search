846385469d9bc824077c4d958078ad97037c9db5
Damien Storm <damien.r.storm@gmail.com>
2015-07-13 20:56:25 +0000

    added status messages to defs

--- libraries/net_ssh.rb
+++ libraries/net_ssh.rb
@@ -1,6 +1,8 @@
 require 'net/ssh'
+require 'colorize'
 def ssh_shutdown(ip)
   command = 'shutdown now -h'
+  puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
   Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
     ssh.exec!(command) rescue nil
   end
@@ -8,6 +10,7 @@ end
 
 def ssh_command(ip, command)
   output = ''
+  puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
   Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
     output = ssh.exec!(command)
   end
@@ -17,6 +20,7 @@ end
 def ssh_rake(ip, data_set = 'all')
   output = ''
   command = "cd ~/amazon_search/ && git pull && git checkout adding_netssh && git pull && rake data_set_#{data_set} local"
+  puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
   Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
     output = ssh.exec!(command)
   end
