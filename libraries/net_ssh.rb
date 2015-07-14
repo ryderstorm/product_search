5043d02b53b99aeba7ea4f0e5938e54ef3ad9c09
Damien Storm <damien.r.storm@gmail.com>
2015-07-14 16:43:30 +0000

    added streaming of ssh output

--- libraries/net_ssh.rb
+++ libraries/net_ssh.rb
@@ -22,13 +22,26 @@ def ssh_command(ip, command)
 end
 
 def ssh_rake(ip, data_set = 'all')
-  output = ''
   command = "cd ~/amazon_search/ && git clean -f && git pull && git checkout adding_netssh -f && git pull && bundle install && rake data_set_#{data_set} local"
   puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
   Net::SSH.start(ip, 'root', :paranoid => false, :timeout => 15) do |ssh|
-    output = ssh.exec!(command)
+    channel = ssh.open_channel do |ch|
+      ch.exec command do |ch, success|
+        raise "could not execute command" unless success
+        # "on_data" is called when the process writes something to stdout
+        ch.on_data do |c, data|
+          $stdout.print data
+        end
+        # "on_extended_data" is called when the process writes something to stderr
+        ch.on_extended_data do |c, type, data|
+          $stderr.print data
+        end
+
+        ch.on_close { puts "done!" }
+      end
+    end
+    channel.wait
   end
-  output
   rescue => e
   puts report_error(e, "Error encountered during ssh_rake")
 end
