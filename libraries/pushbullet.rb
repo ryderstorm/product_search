d3b48577097caf4e676f4f54a2b9d55692a8a983
Damien Storm <damien.r.storm@gmail.com>
2015-07-14 15:19:55 +0000

    removing colorize before sending messages

--- libraries/pushbullet.rb
+++ libraries/pushbullet.rb
@@ -1,4 +1,5 @@
 require 'washbullet'
+require 'colorize'
 
 def pb_client
 	api_key = @secrets[:pushbullet_api_key]
@@ -8,16 +9,16 @@ def pb_client
 end
 
 def pushbullet_note_to_all(title, message,  receivers=nil)
-  pb_client.push_note(receiver:receivers, identifier: '<IDENTIFIER>', params:{title: title, body: message})
+  pb_client.push_note(receiver:receivers, identifier: '<IDENTIFIER>', params:{title: title.uncolorize, body: message.uncolorize})
 end
 
 def pushbullet_file_to_all(title, file, message,  receivers=nil)
   # @client.push_file(@all_devices, title, file, message)
-  pb_client.push_file(receiver:receivers, identifier: '<IDENTIFIER>', params:{file_name: title, file_path: file, body: message})
+  pb_client.push_file(receiver:receivers, identifier: '<IDENTIFIER>', params:{file_name: title.uncolorize, file_path: file, body: message.uncolorize})
 end
 
 def pushbullet_link_to_all(title, link, message,  receivers=nil)
-  pb_client.push_link(receiver:receivers, identifier: '<IDENTIFIER>', params:{title: title, url: link, body: message})
+  pb_client.push_link(receiver:receivers, identifier: '<IDENTIFIER>', params:{title: title.uncolorize, url: link, body: message.uncolorize})
 end
 
 # unless ARGV[0].nil?
