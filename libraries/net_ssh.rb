f399fb4e599a96eea58e202fe945aee919b412a8
Damien Storm <damien.r.storm@gmail.com>
2015-07-14 16:30:41 +0000

    added timeouts

--- libraries/net_ssh.rb
+++ libraries/net_ssh.rb
@@ -3,7 +3,7 @@ require 'colorize'
 def ssh_shutdown(ip)
   command = 'shutdown now -h'
   puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
-  Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
+  Net::SSH.start(ip, 'root', :paranoid => false, :timeout => 15) do |ssh|
     ssh.exec!(command) rescue nil
   end
   rescue => e
@@ -13,7 +13,7 @@ end
 def ssh_command(ip, command)
   output = ''
   puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
-  Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
+  Net::SSH.start(ip, 'root', :paranoid => false, :timeout => 15) do |ssh|
     output = ssh.exec!(command)
   end
   output
@@ -25,7 +25,7 @@ def ssh_rake(ip, data_set = 'all')
   output = ''
   command = "cd ~/amazon_search/ && git pull && git checkout adding_netssh && git pull && bundle install && rake data_set_#{data_set} local"
   puts "Sending the following command to [#{ip.yellow}]: \n" + command.yellow
-  Net::SSH.start(ip, 'root', :paranoid => false) do |ssh|
+  Net::SSH.start(ip, 'root', :paranoid => false, :timeout => 15) do |ssh|
     output = ssh.exec!(command)
   end
   output
