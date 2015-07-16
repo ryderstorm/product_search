a9d3da95ca5c6680c47ddb3da3f757eb642fe9d1
Damien Storm <damien.r.storm@gmail.com>
2015-07-16 21:04:03 +0000

    fixed duration reporting issue

--- libraries/digital_ocean.rb
+++ libraries/digital_ocean.rb
@@ -33,8 +33,8 @@ begin
 	end
 
 	def droplet_status(droplet)
-	  uptime = Time.parse(droplet.created_at).utc.getlocal(-14400) - Time.parse(local_time)
-		puts "#{droplet.name.to_s.light_green.ljust(40)} | #{droplet.status.to_s.center(8).light_red} | " + "$" + hourly_cost(droplet.size_id).to_s.light_cyan + " | #{droplet.id.to_s.light_yellow.ljust(8)} | #{droplet.ip_address.to_s.light_blue.ljust(16)} | #{ seconds_to_string(uptime).light_magenta}"
+	  uptime = Time.now.utc.localtime - Time.parse(droplet.created_at).localtime
+		puts "#{droplet.name.to_s.light_green.ljust(45)} | #{droplet.status.to_s.center(8).light_red} | " + "$" + hourly_cost(droplet.size_id).to_s.light_cyan + " | #{droplet.id.to_s.light_yellow.ljust(8)} | #{droplet.ip_address.to_s.light_blue.ljust(16)} | #{ seconds_to_string(uptime).light_magenta}"
 		puts "\tThis droplet has been running for more than 55 minutes!! Consider shutting it down to avoid additional charge.".light_yellow.on_red if uptime > 3300
 	end
 
